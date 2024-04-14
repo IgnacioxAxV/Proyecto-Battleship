921c1d32c9d1cda685a7917515dcbb7be74036e3
Juan Ignacio <163062195+IgnacioxAxV@users.noreply.github.com>
2024-04-14 01:24:46 -0600

    Se crea la ventana del menu

--- Menu.py
+++ Menu.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 nuevoJuego = {
         "Matriz": {"Columnas": None,"Filas": None, "matrizJ1": [], "matrizJ2": []},
         "NombrePartida": None,
@@ -47,3 +49,55 @@ nuevoJuego = {
             "Acorazado2J2": {"vida": True, "x1": None, "y1": None,"x2": None, "y2": None, "x3": None, "y3": None, "orientacion": None},
             }
     }
+
+def datosPartida():
+    global nuevoJuego
+
+    ventana=Toplevel()
+    ventana.title("Datos de partida")
+    ventana.geometry("410x250+380+280")
+
+    partidaLabel= Label(ventana, text="Inserte el nombre de la partida:")
+    partidaLabel.place(x=1,y=1)
+    nombrePartidaE= Entry(ventana)
+    nombrePartidaE.place(x=270,y=1)
+    nuevoJuego["NombrePartida"]=nombrePartidaE.get()
+
+    jugador1Label= Label(ventana, text="Inserte el nombre del primer jugador:")
+    jugador1Label.place(x=1,y=30)
+    nombreJugador1E= Entry(ventana)
+    nombreJugador1E.place(x=270,y=30)
+    nuevoJuego["Jugador 1"]["Nombre"]=nombreJugador1E.get()
+
+    nickname1Label= Label(ventana, text="Inserte el nombre del primer jugador:")
+    nickname1Label.place(x=1,y=60)
+    nickName1E= Entry(ventana)
+    nickName1E.place(x=270,y=60)
+    nuevoJuego["Jugador 1"]["Nickname"]=nickName1E.get()    
+
+    jugador2Label= Label(ventana, text="Inserte el nombre del segundo jugador:")
+    jugador2Label.place(x=1,y=90)
+    nombreJugador2E= Entry(ventana)
+    nombreJugador2E.place(x=270,y=90)
+    nuevoJuego["Jugador 2"]["Nombre"]=nombreJugador2E.get() 
+
+    nickname2Label= Label(ventana, text="Inserte el nombre del segundo jugador:")
+    nickname2Label.place(x=1,y=120)
+    nickName2E= Entry(ventana)
+    nickName2E.place(x=270,y=120)
+    nuevoJuego["Jugador 2"]["Nickname"]=nickName2E.get()    
+
+    columnaLabel= Label(ventana, text="Columnas:")
+    columnaLabel.place(x=1,y=150)
+    seleccionColumnaE= Spinbox(ventana, from_=20, to=40, increment=2)
+    seleccionColumnaE.place(x=270,y=150)
+    nuevoJuego["Matriz"]["Columnas"]=seleccionColumnaE.get()
+
+    filaLabel= Label(ventana, text="Fila:")
+    filaLabel.place(x=1,y=180)
+    seleccionFilaE= Spinbox(ventana, from_=10, to=40)
+    seleccionFilaE.place(x=270, y=180)
+    nuevoJuego["Matriz"]["Filas"]=seleccionFilaE.get()
+
+    botonEmpezar=Button(ventana, text="Empezar",command="Funcion")
+    botonEmpezar.place(x=310, y=210)
