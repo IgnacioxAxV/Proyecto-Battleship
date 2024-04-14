9c103cf7070d23fa0da4f7541df468a2145359d8
Juan Ignacio <163062195+IgnacioxAxV@users.noreply.github.com>
2024-04-14 01:19:28 -0600

    Se crea un borrador para almacenar los datos de partida

---
+++ Menu.py
@@ -0,0 +1,49 @@
+nuevoJuego = {
+        "Matriz": {"Columnas": None,"Filas": None, "matrizJ1": [], "matrizJ2": []},
+        "NombrePartida": None,
+        "Jugador 1": {
+            "Nombre": None,
+            "Nickname": None,
+            "Puntaje": 0
+        },
+        
+        "Jugador 2": {
+            "Nombre":  None,
+            "Nickname": None,
+            "Puntaje": 0
+        },
+        
+        "BarcosJ1": {
+            "Destructor1J1": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+            "Destructor2J1": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+            "Destructor3J1": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+            "Destructor4J1": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+            "Destructor5J1": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+            "Destructor6J1": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+
+            "Crucero1J1":  {"vida": True, "x1": None, "y1": None, "x2": None , "y2": None, "orientacion": None},
+            "Crucero2J1":  {"vida": True, "x1": None, "y1": None, "x2": None , "y2": None, "orientacion": None},
+            "Crucero3J1":  {"vida": True, "x1": None, "y1": None, "x2": None , "y2": None, "orientacion": None},
+            "Crucero4J1":  {"vida": True, "x1": None, "y1": None, "x2": None , "y2": None, "orientacion": None},
+
+            "Acorazado1J1": {"vida": True, "x1": None, "y1": None,"x2": None, "y2": None,"x3": None, "y3": None, "orientacion": None},
+            "Acorazado2J1": {"vida": True, "x1": None, "y1": None,"x2": None, "y2": None,"x3": None, "y3": None, "orientacion": None}
+            },
+
+        "BarcosJ2": {                   
+            "Destructor1J2": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+            "Destructor2J2": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+            "Destructor3J2": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+            "Destructor4J2": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+            "Destructor5J2": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+            "Destructor6J2": {"vida": True, "x1": None, "y1": None ,"orientacion": None} ,
+
+            "Crucero1J2": {"vida": True, "x1": None, "y1": None,"x2": None, "y2": None, "orientacion": None},
+            "Crucero2J2": {"vida": True, "x1": None, "y1": None,"x2": None, "y2": None, "orientacion": None},
+            "Crucero3J2": {"vida": True, "x1": None, "y1": None,"x2": None, "y2": None, "orientacion": None},
+            "Crucero4J2": {"vida": True, "x1": None, "y1": None,"x2": None, "y2": None, "orientacion": None},
+
+            "Acorazado1J2": {"vida": True, "x1": None, "y1": None,"x2": None, "y2": None, "x3": None, "y3": None, "orientacion": None},
+            "Acorazado2J2": {"vida": True, "x1": None, "y1": None,"x2": None, "y2": None, "x3": None, "y3": None, "orientacion": None},
+            }
+    }
